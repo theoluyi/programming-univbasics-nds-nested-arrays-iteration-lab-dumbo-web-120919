1da6be3a42e80a54213dfa79da743e5278e9dcf1
Theo Carney <theoluyi@gmail.com>
2019-12-04 20:36:01 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,12 +49,10 @@ def total_even_pairs(src)
   i = 0 
   
   while i < src.length do 
-    if src[i][0]%2 == 0 
-      total += src[i][0]
+    if src[i][0]%2 == 0 && src[i][1]%2 == 0 
+      total += src[i][0] + src[i][1]
     end
-    if src[i][1]%2 == 0 
-      total += src[i][1]
-    end 
+
   i += 1 
   end
   
